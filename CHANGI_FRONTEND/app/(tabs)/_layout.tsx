9f6db9777ea52a9fcc26f6fd6012634788585944
vdr-deloitte-ypui <vdr-deloitte.ypui@wrs.com.sg>
2024-10-09 14:42:59 +0800

    Added bottom sheet with placholder content

--- CHANGI_FRONTEND/app/(tabs)/_layout.tsx
+++ CHANGI_FRONTEND/app/(tabs)/_layout.tsx
@@ -1,37 +1,29 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Stack, Tabs } from 'expo-router';
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+    <Stack
+    screenOptions={{
+        // Hide the header for all other routes.
         headerShown: false,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
-          ),
-        }}
-      />
-    </Tabs>
+      }}
+    >
+        <Stack.Screen name="index" 
+        options={{ 
+            headerShown: false
+         }} />
+    </Stack>
+    // <Tabs
+    // screenOptions={{
+    //     headerShown: false,
+    // }}
+    // >
+    //     <Tabs.Screen redirect 
+    //     name="index"
+    //     options={{
+    //         href: null,
+    //       }}
+    //     />
+    // </Tabs>
   );
-}
+};
--- CHANGI_FRONTEND/app/_layout.tsx
+++ CHANGI_FRONTEND/app/_layout.tsx
@@ -1,40 +1,58 @@
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+
+import fonts from '@/constants/fonts';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 // SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
 
-  // const [loaded, error] = useFonts(fonts);
-
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const [loaded, error] = useFonts(fonts);
 
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
     }
-  }, [loaded]);
+  }, [loaded, error]);
 
-  if (!loaded) {
+  if (!loaded && !error) {
     return null;
   }
 
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 5,
+        retryDelay: 1000
+      },
+    }
+  }
+  );
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Stack>
+          <Stack.Screen 
+          name="(tabs)"
+          options={{
+            headerShown: false
+          }}
+          />
+        </Stack>
+      </GestureHandlerRootView>
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    </QueryClientProvider>
   );
 }
